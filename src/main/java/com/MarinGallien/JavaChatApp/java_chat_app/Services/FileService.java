1e76ad28298bbd800bf3e2dad7894f5accd6b036
Marin <gallienmarin2001@gmail.com>
2025-06-23 17:57:45 -0700

    file service

--- src/main/java/com/MarinGallien/JavaChatApp/java_chat_app/Services/FileService.java
+++ src/main/java/com/MarinGallien/JavaChatApp/java_chat_app/Services/FileService.java
@@ -1,9 +1,11 @@
 package com.MarinGallien.JavaChatApp.java_chat_app.Services;
 
+import com.MarinGallien.JavaChatApp.java_chat_app.Database.DatabaseServices.FileDbService;
 import com.MarinGallien.JavaChatApp.java_chat_app.Database.JPAEntities.CoreEntities.File;
-import jakarta.annotation.Resource;
+import org.springframework.core.io.Resource;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -14,22 +16,35 @@ public class FileService {
 
     private static Logger logger = LoggerFactory.getLogger(FileService.class);
 
-    public File uploadFile(String userId, String chatId, MultipartFile file) {
+    @Autowired
+    FileDbService fileDbService;
+
+    public boolean uploadFile(String userId, String chatId, MultipartFile file) {
         try {
             // Validate inputs
-            if (!validateId(userId)) {
-                logger.warn("Failed to upload file: user ID is null or empty");
-                return null;
+            if (!validateId(userId) || !validateId(chatId)) {
+                logger.warn("Failed to upload file: user or chat ID is null or empty");
+                return false;
             }
 
             if (file == null || file.isEmpty()) {
                 logger.warn("Failed to upload file: file is null or empty");
-                return null;
+                return false;
             }
+
+            File uploadedFile = fileDbService.uploadFile(userId, chatId, file);
+
+            if (uploadedFile == null) {
+                logger.warn("Failed to upload file");
+                return false;
+            }
+
+            logger.info("Successfully uploaded file");
+            return true;
 
         } catch (Exception e) {
             logger.error("Failed to upload file: {}", e.getMessage());
-            return null;
+            return false;
         }
     }
 
@@ -41,6 +56,15 @@ public class FileService {
                 return null;
             }
 
+            Resource resource = fileDbService.downloadFile(userId, chatId, fileId);
+
+            if (resource == null) {
+                logger.warn("Failed to download file");
+                return null;
+            }
+
+            logger.info("Successfully downloading file to client");
+            return resource;
 
         } catch (Exception e) {
             logger.error("Failed to download file: {}", e.getMessage());
@@ -56,6 +80,15 @@ public class FileService {
                 return false;
             }
 
+            boolean deleted = fileDbService.deleteFile(userId, chatId, fileId);
+
+            if (!deleted) {
+                logger.warn("Failed to delete file");
+                return false;
+            }
+
+            logger.info("Successfully deleted file");
+            return true;
 
         } catch (Exception e) {
             logger.error("Failed to delete file: {}", e.getMessage());
@@ -67,11 +100,19 @@ public class FileService {
         try {
             // Validate inputs
             if (!validateId(userId) || !validateId(chatId)) {
-                logger.warn("Failed to retrieve files list: user or file ID is null or empty");
+                logger.warn("Failed to retrieve files list: user or chat ID is null or empty");
                 return null;
             }
 
+            List<File> chatFiles = fileDbService.getChatFiles(userId, chatId);
 
+            if (chatFiles == null) {
+                logger.warn("Failed to retrieve list of files from chat {}", chatId);
+                return null;
+            }
+
+            logger.info("Successfully retrieved list of files from chat {}", chatId);
+            return chatFiles;
 
         } catch (Exception e) {
             logger.error("Failed to retrieve files list: {}", e.getMessage());
